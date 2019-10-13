f5cb144e25a92fdafa26658c36177d92861f6e69
hy1714317856 <1714317856@qq.com>
2019-10-12 22:16:09 -0700

    week five work add

---
+++ code/lib/fun1.c
@@ -0,0 +1,11 @@
+#include "lib.h"
+void getnumber(int *a,int n)
+{
+	int i;
+  	srand((int)time(0));
+  	for(i=0;i<n;i++)
+	{	
+		a[i]=1+(int)(100.0*rand()/(RAND_MAX+1.0));
+		printf("%d\n",a[i]);	
+	}
+}
---
+++ code/lib/lib.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void getnumber(int*,int);
+int max(int *,int);
+int sum(int *,int);
+
+
