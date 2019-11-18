54570862a2ec2ba8778302185f59a18eeb7c1b88
hy1714317856 <1714317856@qq.com>
2019-11-18 00:59:36 -0800

    week11

---
+++ linuxprogram/week11/code/test6.c
@@ -0,0 +1,40 @@
+#include "my.h"
+
+void pr_exit(int status,int pid){
+	int sig;
+	if(WIFEXITED(status))
+		printf("process %d nomal termination,exot status =%d\n",pid,WIFEXITED(status));
+	else if(WIFSIGNALED(status)){
+		sig = WTERMSIG(status);
+		printf("process %d abnomal termination,exit signal number =%d%s\n",pid,sig,
+		#ifdef WCOREDUMO
+			WCOREDUMP(status)?"(core file generated)":"");
+		#else
+			"");
+		#endif
+			psignal(sig,"");
+	}
+	else if(WIFSTOPPED(status)){
+		sig = WSTOPSIG(status);
+		printf("process %d stopped,signal number =%d\n",pid,sig);
+		psignal(sig,"");
+	}
+}
+
+
+int zero =0;
+int main(){
+	int pid,status,r;
+	if((pid=fork())==0)
+		exit(0);
+	if((pid=fork())==0)
+		abort();
+	if((pid=fork())==0){
+		status = status/0;
+		exit(0);
+	}
+	while((pid=wait(&status))>=0)
+		pr_exit(status,pid);
+	perror("parent wait over!\n");
+	exit(0);
+}
